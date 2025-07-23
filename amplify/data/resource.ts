219b65e1637b5cb3d0f64395f4381699494e37e0
aponnudurai-cfn <164194534+aponnudurai-cfn@users.noreply.github.com>
2025-07-23 09:23:43 -0400

    Update resource.ts

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,5 +1,4 @@
 import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
-import { identifyUser } from "aws-amplify/analytics";
 
 /*== STEP 1 ===============================================================
 The section below creates a Todo database table with a "content" field. Try
