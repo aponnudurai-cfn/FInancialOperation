2479fac64e16d4a318809205a482381a6d524783
aponnudurai-cfn <164194534+aponnudurai-cfn@users.noreply.github.com>
2025-07-25 16:31:55 -0400

    Update echo.ts

--- amplify/data/echo/echo.ts
+++ amplify/data/echo/echo.ts
@@ -1,24 +1,8 @@
-import type { Schema } from "../resource";
-
-// manually type the function context
-export const handler = async (
-  context: {
-    arguments: {
-      content?: string;
-    };
-    db: {
-      Rating: {
-        list: () => Promise<{ items: { id: string }[] }>;
-      };
-    };
-  }
-) => {
+import type { Schema } from '../resource'
+export const handler: Schema["echo"]["functionHandler"] = async (event, context) => {
   const start = performance.now();
-
-  const { items: ratings } = await context.db.Rating.list();
-
   return {
-    content: `Echoing content: ${context.arguments.content} and found ${ratings.length} ratings`,
-    executionDuration: performance.now() - start,
+    content: `Echoing content: ${event.arguments.content}`,
+    executionDuration: performance.now() - start
   };
 };
